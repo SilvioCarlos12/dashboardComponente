ec974ea9419d8367fdf79dc36d27667f96637852
SilvioCarlos12 <silvio.net.ma@gmail.com>
2022-10-16 23:46:31 -0300

    fix:correção de returno

---
+++ app/lib/componentes/dashboard/chartJs/BarChartJs.php
@@ -0,0 +1,43 @@
+<?php
+
+
+use Adianti\Widget\Template\THtmlRenderer;
+
+class BarChartJs implements  BarChart{
+
+    private $altura;
+    private $largura;
+    private $titulo;
+    private $id;
+    private $dados;
+
+    function __construct($altura,$largura,$id,$titulo){
+        $this->altura=$altura;
+        $this->largura=$largura;
+        $this->titulo=$titulo;
+        $this->id=$id;
+    }
+
+
+    public function ConfiguracaoBarChart(): array
+    {
+        return
+        ['data'   => json_encode($this->dados),
+        'width'  => $this->largura,
+        'height'  => $this->altura,
+        'title'  => $this->titulo,
+        'uniqid' => $this->id];
+    }
+
+    public function DadosParaChart(array $dados)
+    {
+        $this->dados=$dados;
+    }
+    public function CriarChartBar(): THtmlRenderer
+    {
+        if($this->dados == null) throw new Exception("Preciso passa dados pelo DadosParaChart");
+        $barChart=new THtmlRenderer('app/resources/charjs_bar.html');
+        $barChart->enableSection("main",$this->ConfiguracaoBarChart());
+        return $barChart;
+    }
+}
